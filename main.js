6fb589b2e958a9c5532f425d4a31fba5d828287e
issahsamori <116550672+issahsamori@users.noreply.github.com>
2023-11-24 18:38:49 +0000

    setup basic presentation styles

--- main.js
+++ main.js
@@ -5,15 +5,19 @@
 class Presentation {
   static SLIDE_ATTR = "data-slide"
   static CURRENT_SLIDE_ATTR = "data-current"
+  static FRAGMENT_ATTR = "data-fragment"
+  static ACTIVE_FRAGMENT_ATTR = "data-active"
 
   /** @type {number} */
   #currentIdx
   /** @type {number} */
-  #prevIdx
+  #activeFragmentIdx
   /** @type {HTMLElement} */
   #presentationEl
   /** @type {HTMLElement[]} */
   #slideEls
+  /** @type {HTMLElement[]} */
+  #currFragmentEls
   /** @type {number} */
   #width
   /** @type {number} */
@@ -32,18 +36,18 @@ class Presentation {
 
     this.currentIdx = 0
 
+    this.#presentationEl.style.setProperty("--slide-width", this.#width.toString())
+    this.#presentationEl.style.setProperty("--slide-height", (this.#width / this.#aspectRatio).toString())
+    this.#presentationEl.style.setProperty("--slide-scale", "1")
+
     this.#handleResize = () => {
       const presentationRect = this.#presentationEl.getBoundingClientRect()
       const domAspectRatio = presentationRect.width/presentationRect.height
-      console.log(this.#aspectRatio, domAspectRatio)
       if(domAspectRatio <= this.#aspectRatio) {
         this.#presentationEl.style.setProperty("--slide-scale", (presentationRect.width/ this.#width).toString())
       } else {
         this.#presentationEl.style.setProperty("--slide-scale", (presentationRect.height/ (this.#width / this.#aspectRatio)).toString())
       }
-  
-      this.#presentationEl.style.setProperty("--slide-width", this.#width.toString())
-      this.#presentationEl.style.setProperty("--slide-height", (this.#width / this.#aspectRatio).toString())
     }
   }
 
@@ -51,18 +55,52 @@ class Presentation {
     return this.#slideEls.length
   }
 
+  get #currTotalFragmentCount() {
+    return this.#currFragmentEls.length
+  }
+
   get currentIdx() {
     return this.#currentIdx
   }
 
   set currentIdx(idx) {
-    this.#prevIdx = this.#currentIdx
-    this.#currentIdx = Math.min(Math.max(idx, 0), this.#totalSlideCount)
-    if(this.#prevIdx != null) {
-      this.#slideEls[this.#prevIdx].removeAttribute(Presentation.CURRENT_SLIDE_ATTR)
+    if(this.#totalSlideCount <= 0) return
+
+    const prevIdx = this.#currentIdx
+    this.#currentIdx = Math.min(Math.max(idx, 0), this.#totalSlideCount - 1)
+    if(this.#currentIdx === prevIdx) return
+
+    if(prevIdx != null) {
+      this.#slideEls[prevIdx].removeAttribute(Presentation.CURRENT_SLIDE_ATTR)
     }
 
     this.#slideEls[this.#currentIdx].setAttribute(Presentation.CURRENT_SLIDE_ATTR, "")
+
+    this.#currFragmentEls = Array
+      .from(this.#slideEls[this.#currentIdx].querySelectorAll(`[${Presentation.FRAGMENT_ATTR}]`))
+      .filter(el => el.closest(`[${Presentation.SLIDE_ATTR}]`) === this.#slideEls[this.#currentIdx])
+    this.activeFragmentIdx = prevIdx < this.#currentIdx
+      ? -1 : this.#currTotalFragmentCount - 1
+  }
+
+  get activeFragmentIdx() {
+    return this.#activeFragmentIdx
+  }
+
+  set activeFragmentIdx(idx) {
+    if(this.#currTotalFragmentCount <= 0) return
+    
+    const prevIdx = this.#activeFragmentIdx
+    this.#activeFragmentIdx = Math.min(Math.max(idx, -1), this.#currTotalFragmentCount - 1)
+    if(this.#activeFragmentIdx === prevIdx) return
+
+    if(prevIdx !== -1) {
+      this.#currFragmentEls[prevIdx]?.removeAttribute(Presentation.ACTIVE_FRAGMENT_ATTR)
+    }
+
+    if(this.#activeFragmentIdx === -1) return
+
+    this.#currFragmentEls[this.#activeFragmentIdx]?.setAttribute(Presentation.ACTIVE_FRAGMENT_ATTR, "")
   }
 
   init() {
@@ -73,8 +111,56 @@ class Presentation {
   destroy() {
     window.removeEventListener("resize", this.#handleResize)
   }
+
+  advance() {
+    if(!this.advanceFragment()) this.advanceSlide()
+  }
+
+  advanceSlide() {
+    const prevIdx = this.#currentIdx
+    this.currentIdx += 1
+    return this.#currentIdx !== prevIdx
+  }
+
+  advanceFragment() {
+    const prevIdx = this.#activeFragmentIdx
+    this.activeFragmentIdx += 1
+    return this.#activeFragmentIdx !== prevIdx
+  }
+
+  retreat() {
+    if(!this.retreatFragment()) this.retreatSlide()
+  }
+
+  retreatSlide() {
+    const prevIdx = this.#currentIdx
+    this.currentIdx -= 1
+    return this.#currentIdx !== prevIdx
+  }
+
+  retreatFragment() {
+    const prevIdx = this.#activeFragmentIdx
+    this.activeFragmentIdx -= 1
+    return this.#activeFragmentIdx !== prevIdx
+  }
 }
 
-const presentation = new Presentation(document.querySelector("[data-presentation]"), { width: 600 })
+const presentation = new Presentation(document.querySelector("[data-presentation]"))
 presentation.init()
-console.log(presentation)
+document.querySelector("[data-control-next]")?.addEventListener("click", () => {
+  presentation.advance()
+})
+document.querySelector("[data-control-prev]")?.addEventListener("click", () => {
+  presentation.retreat()
+})
+document.querySelector("[data-control-full-screen]")?.addEventListener("click", () => {
+  toggleFullScreen()
+})
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
